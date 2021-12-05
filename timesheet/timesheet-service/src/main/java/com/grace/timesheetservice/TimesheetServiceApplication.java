73f83b165253dd6336f44f9c089d577d97bf6743
Aju Mathews <aju.mathewsamuel@gmail.com>
2021-12-05 14:07:04 +0530

    Initialised Timesheet service

---
+++ timesheet/timesheet-service/src/main/java/com/grace/timesheetservice/TimesheetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grace.timesheetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetServiceApplication.class, args);
+	}
+
+}
